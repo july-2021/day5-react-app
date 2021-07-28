b355dde96d049f61235fecc58a7e66f770f20a2c
santosh <santosh.ece06@gmail.com>
2021-07-28 19:24:43 +0530

    working with list and events.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import "./App.css";
 function App() {
   let list = ["Hello", "Hello"];
 
+  const addNewHello = () => {
+    // alert("Add new Hello logic weill come here....");
+    list.push("Hello");
+
+    console.log(list);
+  };
+
+  // VIEW
+  // React is unware of the changes of local variable
   return (
     <div>
       <h1>Working with Events and List</h1>
-      <input type="button" value="Say Hello" />
+      <input type="button" value="Say Hello" onClick={addNewHello} />
 
       {list.map((item) => {
         return <h3>{item}</h3>;
