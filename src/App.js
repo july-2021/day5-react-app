dccdc1e0a80380c50adb574a77a769d74f8feaaa
santosh <santosh.ece06@gmail.com>
2021-07-28 18:42:11 +0530

    b1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+// JSX => JAVSCRIPT XML => JS + HTML
+// JSX must be wrapped within () if it is multiline
+
 function App() {
-  return <div className="App">Helllo world!!!!</div>;
+  return <h1>Hello universe</h1>;
 }
 
 export default App;
