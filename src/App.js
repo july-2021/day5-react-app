fc6f2f127fd3d6611446df5922b1e84c8ed8be86
santosh <santosh.ece06@gmail.com>
2021-07-28 18:53:55 +0530

    working with variable.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import "./App.css";
 // JSX must be wrapped within () if it is multiline
 // JSX can have only one PARENT tag.
 function App() {
+  let id = 100;
+  let name = "Sachin";
+
+  // VIEW => That is getting render in the browser.
+  // INSIDE THE HTML you can access the JS VARIABLE.
+  // using the {} you can write ANY JAVASCRIPT PROGRAM.
   return (
     <div>
-      <h1>Thank you</h1>
+      <h1>
+        {name} {id}
+      </h1>
       <h1>Multi line Statement. () is important now</h1>
     </div>
   );
