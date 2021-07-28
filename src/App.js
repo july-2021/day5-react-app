2de97058ce5957eef68040cd2f5df0cdd9c55ae6
santosh <santosh.ece06@gmail.com>
2021-07-28 18:45:40 +0530

    mulitline.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import "./App.css";
 // JSX must be wrapped within () if it is multiline
 
 function App() {
-  return <h1>Hello universe</h1>;
+  return (
+    <div>
+      <h1>Thank you</h1>
+      <h1>Multi line Statement. () is important now</h1>
+    </div>
+  );
 }
 
 export default App;
