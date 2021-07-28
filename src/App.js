54494f381776f56b3fef6933f7b27e89f54f165b
santosh <santosh.ece06@gmail.com>
2021-07-28 18:57:39 +0530

    basic raw list.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-// JSX => JAVSCRIPT XML => JS + HTML
-// JSX must be wrapped within () if it is multiline
-// JSX can have only one PARENT tag.
 function App() {
-  let id = 100;
-  let name = "Sachin";
+  let title = "Hello React;";
+  let list = ["Delhi", "Calcutta", "Mumbai", "Chennai"];
 
-  // VIEW => That is getting render in the browser.
-  // INSIDE THE HTML you can access the JS VARIABLE.
-  // using the {} you can write ANY JAVASCRIPT PROGRAM.
   return (
     <div>
-      <h1>
-        {name} {id}
-      </h1>
-      <h1>Multi line Statement. () is important now</h1>
+      <h1>{title}</h1>
+      <h1>{list}</h1>
     </div>
   );
 }
